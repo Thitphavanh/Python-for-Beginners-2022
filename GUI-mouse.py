f33ae349826a59bf634c1bbc677cc1c77dfb105b
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-23 19:16:09 +0700

    update

--- GUI-mouse.py
+++ GUI-mouse.py
@@ -1,5 +1,5 @@
 # GUI-mouse.py
-from tkinter import * 
+from tkinter import *
 from tkinter import ttk
 import pyautogui as pg
 import webbrowser
@@ -8,19 +8,23 @@ GUI = Tk()
 GUI.title('Click calendar program')
 GUI.geometry('500x300')
 
+
 def Calendar():
-	pg.click(1814, 1062)
+    pg.click(1814, 1062)
 
-Button1 = Button(GUI, text = 'Calendar1', command = Calendar)
-Button1.pack(ipadx = 20, ipady = 10, pady = 20)
 
-Button2 = ttk.Button(GUI, text = 'Calendar2', command = Calendar)
-Button2.pack(ipadx = 20, ipady = 10, pady = 20)
+Button1 = Button(GUI, text='Calendar1', command=Calendar)
+Button1.pack(ipadx=20, ipady=10, pady=20)
+
+Button2 = ttk.Button(GUI, text='Calendar2', command=Calendar)
+Button2.pack(ipadx=20, ipady=10, pady=20)
+
 
 def Google():
-	webbrowser.open('https://www.google.com')
+    webbrowser.open('https://www.google.com')
 
-Button3 = ttk.Button(GUI, text = 'Google', command = Google)
-Button3.pack(ipadx = 20, ipady = 10)
+
+Button3 = ttk.Button(GUI, text='Google', command=Google)
+Button3.pack(ipadx=20, ipady=10)
 
 GUI.mainloop()
--- basic_input.py
+++ basic_input.py
@@ -1,8 +1,7 @@
 friend = []
 
 for i in range(5):
-	data = input('What is your friend name : ')
-	friend.append(data)
+    data = input('What is your friend name : ')
+    friend.append(data)
 
 print(friend)
-
