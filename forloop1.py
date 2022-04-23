17e0f33a215971c2f1a443e06aa1cea3cdeb5215
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-23 21:27:15 +0700

    update

---
+++ forloop1.py
@@ -0,0 +1,38 @@
+# forloop1.py
+
+# loop ຕາມຈຳນວນ
+for i in range(10):
+    print('Hello :', i)
+
+print('--------Loop in list--------')
+friends = ['James', 'Noy', 'Tui', 'Ley', 'Jo']
+
+for f in friends:
+    print(f)
+    if f == 'James':
+        print('ສະບາຍດີ ເຈມສ໌')
+    else:
+        print('ສະບາຍດີ ລູກຄ້າ')
+print('--------Loop in Dictionary--------')
+friends2 = {'James': 'ເຈມສ໌', 'Noy': 'ນ້ອຍ', 'Tui': 'ຕຸ້ຍ', 'Ley': 'ເລ້'}
+
+# Show key
+for f in friends2:
+    print(f)
+
+# Show items
+for k, v in friends2.items():
+    print('keys :', k)
+    print('values :', v)
+
+# Show key only
+for f in friends2.keys():
+    print(f)
+
+# Show vulues only
+for f in friends2.values():
+    print(f)
+
+# ຕ້ອງການລຳດັບ
+for i, f in enumerate(friends, start=1000):
+    print(i, f)
--- while1.py
+++ while1.py
@@ -1,1 +1,15 @@
 # while1.py
+
+money = 100000
+transfer = 1000000
+
+print('Check : ', money < transfer)
+
+while money < transfer:
+	print('ກະລຸນາຕື່ມໂຕເລກໂອນໃໝ່ເຈົ້າ')
+	transfer = int(input('new transfer : '))
+	if transfer > 10000000:
+		print('ເອີ່ນຜູ້ຈັດການມາ ຂໍເຄລຍຣ໌ກ່ອນຈຶ່ງຈະໂອນໄດ້')
+		break
+
+print('ໂອນໄດ້ຖ້າຜູ້ຈັດການອະນຸຍາດສຳຫຼັບກໍລະນີເປັນ VIP')
