7c2eeb76467c2d37958a78f37ee5421790478b76
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-23 20:31:28 +0700

    update

--- ifelse2.py
+++ ifelse2.py
@@ -1,4 +1,15 @@
 # ifelse2.py
-friends = ['James','Noy','Tui','Ley']
+friends = ['James', 'Noy', 'Tui', 'Ley', 'Jo']
 
-visitor = ''
+friends2 = {'James': 'ເຈມສ໌', 'Noy': 'ນ້ອຍ', 'Tui': 'ຕຸ້ຍ', 'Ley': 'ເລ້'}
+
+visitor = 'jo'
+
+if visitor in friends or visitor.title() in friends:
+    print('ເປັນໝູ່ຂ້ອຍເອງ ເຊີນຂຶ້ນມາໄດ້ເລີຍ')
+    if visitor in friends2 or visitor.title() in friends2:
+        print('ສະບາຍດີ ' + friends2[visitor.title()])
+    else:
+        print('ສະບາຍດີ ລູກຄ້າ')
+else:
+    print('Hey! ເຈົ້າແມ່ນໃຜ ບໍ່ມີຊື່ໃນ Member')
