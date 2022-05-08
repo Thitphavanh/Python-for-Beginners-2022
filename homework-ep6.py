ba1e3b35b168f576992ad58c4125b1c93338e869
Thitphavanh <thitphavanh.psv@gmail.com>
2022-05-08 07:50:39 +0700

    update

---
+++ homework-ep6.py
@@ -0,0 +1,18 @@
+premierleagueteams = ['Liverpool', 'Manchester City']
+
+teams1 = 'Manchester City'
+teams2 = 'Liverpool'
+
+win = 5
+
+champion = 'Premier League of Champion'
+
+chioce = 'will must all win in the last 6 games'
+
+
+if (win >= 6):
+    print(teams1, champion)
+elif (win >= 5):
+    print('but', teams2, chioce, '=', teams2, champion)
+
+print('congratulations')
