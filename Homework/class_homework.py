a944fff075496a7d83d096f71b83e28f4389a73d
Thitphavanh <thitphavanh.psv@gmail.com>
2022-05-21 09:31:32 +0700

    update

---
+++ Homework/class_homework.py
@@ -0,0 +1,77 @@
+# class PublicEmp:
+# 	def __init__(self, salary):
+# 		self.salary = salary
+
+
+# class ProtectedEmp:
+# 	def __init__(self, salary):
+# 		self._salary = salary
+
+
+# class Emp(ProtectedEmp):
+# 	def getsalary(self):
+# 		return self._salary
+
+
+# class PrivateEmp:
+# 	def __init__(self, salary):
+# 		self._salary = salary
+
+# 	def getsalary(self):
+# 		return self._salary
+
+
+# myEmp1 = PublicEmp(1800000)
+# print('PublicEmp salary = {0:,.2f} LAK'.format(myEmp1.salary))
+
+
+# myEmp2 = PublicEmp(2800000)
+# print('ProtectedEmp salary = {0:,.2f} LAK'.format(myEmp2.salary))
+
+# myEmp3 = PublicEmp(2500000)
+# print('PrivateEmp salary = {0:,.2f} LAK'.format(myEmp3.salary))
+
+
+class Employee:
+    minSalary = 2800000
+    maxSalary = 4000000
+    _companyName = "Anitocorn"
+
+    def __init__(self, name, salary, department):
+        self.name = name
+        self.salary = salary
+        self.department = department
+
+    def _showData(self):
+        print("ຊື່ພະນັກງານ = {}".format(self.name))
+        print("ເງິນເດືອນ = {0:,.2f}".format(self.salary))
+        print("ຕຳແໜ່ງ = {}".format(self.department))
+
+    def _getIncome(self):
+        return self.salary * 12
+
+    def __str__(self):
+        return ("ຊື່ພະນັກງານ = {} , ເງິນເດືອນ = {} , ຕຳແໜ່ງ = {}".format(self.name, self.salary, self.department))
+
+
+class Accounting(Employee):
+    __departmentName = "ພະແນກບັນຊີ"
+
+    def __init__(self, name, salary):
+        super().__init__(name, salary, self.__departmentName)
+
+
+class Programmer(Employee):
+    __departmentName = "ໂປຣແກຣມເມີຣ໌"
+
+    def __init__(self, name, salary, experience):
+        super().__init__(name, salary, self.__departmentName)
+        self.exp = experience
+
+    def __str__(self):
+        return (super().__str__()+" , ປະສົບການເຮັດວຽກ = {} ປີ".format(self.exp))
+
+
+programmer = Programmer("James", 4000000, 2)
+print(programmer.__str__())
+print("ລາຍໄດ້ຕໍ່ປີ = {}".format(programmer._getIncome()))
--- Web Scraping/temperature.py
+++ Web Scraping/temperature.py
@@ -1,5 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+
+
+
+def writeToCsv(data):
+	date = datetime.now().strftime('%Y-%m-%d')
+	with open('data-temperature-{}.csv'.format(date), 'a', newline='', encoding='utf-8') as file:
+		fileWriter = csv.writer(file)
+		fileWriter.writerow(data)
+
 
 alldata = {}
 
@@ -29,4 +40,10 @@ def checktemp(ID):
 for i in range(1, 101):
 	checktemp(i)
 
-print(alldata['มุกดาหาร'])
+# print(alldata['มุกดาหาร'])
+
+for k,v in alldata.items():
+	data = [k,v]
+	writeToCsv(data)
+
+print('saved')
