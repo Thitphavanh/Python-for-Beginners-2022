5a5d3992089032c2a460a7483b192aee8542fd2e
Hery <thitphavanh.psv@gmail.com>
2022-06-25 21:40:52 +0700

    update

--- Django Project/mywebsite/myapp/models.py
+++ Django Project/mywebsite/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.FloatField()
+	detail = models.CharField(max_length=300)
+
+	def __str__(self):
+		return self.name
+
+
+class ContactList(models.Model):
+	email = models.CharField(max_length=100)
+	title = models.CharField(max_length=200)
+	detail = models.CharField(max_length=300)
+
+	def __str__(self):
+		return self.email + ' ' + self.title
--- Django Project/mywebsite/myapp/views.py
+++ Django Project/mywebsite/myapp/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def hello(request):
 	return HttpResponse('<h1>Hello World</h1>')
 
 def homepage(request):
-	return render(request,'myapp/home.html')
+	products = Product.objects.all()
+	allproducts = []
+	column = []
+
+	for i,p in enumerate(products, start=1):
+		if i % 3==0:
+			column.append(p)
+			allproducts.append(column)
+			column = []
+
+		else:
+			column.append(p)
+
+	if len(column) != 0:
+		allproducts.append(column)
+
+	context = {'products':allproducts}
+		
+
+			
+	return render(request,'myapp/home.html', context)
+	
 
 
 def about(request):
