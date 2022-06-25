1740e4ec442031ba4b9796392dbe100f9e0ed359
Hery <thitphavanh.psv@gmail.com>
2022-06-25 22:17:56 +0700

    update

--- Django Project/mywebsite/myapp/views.py
+++ Django Project/mywebsite/myapp/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
+
 def hello(request):
 	return HttpResponse('<h1>Hello World</h1>')
+
 
 def homepage(request):
 	products = Product.objects.all()
 	allproducts = []
 	column = []
 
-	for i,p in enumerate(products, start=1):
-		if i % 3==0:
+	for i, p in enumerate(products, start=1):
+		if i % 3 == 0:
 			column.append(p)
 			allproducts.append(column)
 			column = []
@@ -22,18 +24,26 @@ def homepage(request):
 	if len(column) != 0:
 		allproducts.append(column)
 
-	context = {'products':allproducts}
-		
+	context = {'products': allproducts}
 
-			
-	return render(request,'myapp/home.html', context)
-	
+	return render(request, 'myapp/home.html', context)
 
 
 def about(request):
-	return render(request,'myapp/about.html')
+	return render(request, 'myapp/about.html')
 
 
 def contact(request):
-	return render(request,'myapp/contact.html')
+	if request.method == 'POST':
+		data = request.POST.copy()
+		email = data.get('email')
+		title = data.get('title')
+		detail = data.get('detail')
 
+		newcontact = ContactList()
+		newcontact.email = email
+		newcontact.title = title
+		newcontact.detail = detail
+		newcontact.save()
+
+	return render(request, 'myapp/contact.html')
