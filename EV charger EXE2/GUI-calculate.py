8b052f562691b2276e0ece4311f8bb2ebc9cbc1c
Hery <thitphavanh.psv@gmail.com>
2022-08-06 14:14:23 +0700

    update

---
+++ EV charger EXE2/GUI-calculate.py
@@ -0,0 +1,42 @@
+# GUI-calculate.py
+
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Program charge bettery')
+GUI.geometry('1200x800')
+
+
+bg = PhotoImage(file='electric-car4.png')
+
+BG = Label(GUI, image=bg)
+BG.pack(pady=5)
+
+label = Label(GUI, text='Please entry minute in this field',
+              font=(None, 18))
+label.pack(pady=5)
+
+v_quantity = StringVar()  # ເປັນໂຕແປທີ່ໃຊ້ເກັບຂໍ້ຄວາມເມື່ອພິມແລ້ວ
+Entry1 = ttk.Entry(GUI, textvariable=v_quantity, font=(None, 18))
+Entry1.pack(pady=5)
+
+
+def Calculate():
+    try:
+        quantity = float(v_quantity.get())
+        calculate = quantity * 1500
+        messagebox.showinfo(
+            'Total value', 'Total value {} LAK'.format(calculate))
+        v_quantity.set('')
+        Entry1.focus()
+    except:
+        messagebox.showerror('You filled in wrong', 'Only fill in quantity')
+        v_quantity.set('')
+        Entry1.focus()
+
+
+Button1 = ttk.Button(GUI, text='Calculate', command=Calculate)
+Button1.pack(ipadx=20, ipady=10, pady=10)
+
+GUI.mainloop()
